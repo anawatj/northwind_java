07cc2ea09511d62c82f77475c8006f7284406d39
anawatj <ajarusiripot@gmail.com>
2015-11-19 12:49:08 +0700

    getAllCity

--- northwindj/northwindj/src/main/java/com/tao/northwindj/repositories/imp/MasterRepository.java
+++ northwindj/northwindj/src/main/java/com/tao/northwindj/repositories/imp/MasterRepository.java
@@ -1,7 +1,12 @@
 package com.tao.northwindj.repositories.imp;
 
+import java.util.List;
+import java.util.Map;
+import java.util.Vector;
+
 import org.hibernate.Criteria;
 import org.hibernate.SessionFactory;
+import org.hibernate.criterion.Restrictions;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
@@ -18,6 +23,17 @@ public class MasterRepository {
 	public Result<SimpleMasterObject> findAll(Class clazz)
 	{
 		Criteria criteria = this.factory.getCurrentSession().createCriteria(clazz);
+		return new Result<SimpleMasterObject>(criteria);
+		
+		
+	}
+	public Result<SimpleMasterObject> findAll(Class clazz,Vector<String> fields,Vector<Object> values)
+	{
+		Criteria criteria =  this.factory.getCurrentSession().createCriteria(clazz);
+		for(int index=0;index<fields.size();index++)
+		{
+			criteria.add(Restrictions.eq(fields.get(index), values.get(index)));
+		}
 		return new Result<SimpleMasterObject>(criteria);
 		
 		
--- northwindj/northwindj/src/main/java/com/tao/northwindj/web/controllers/MasterController.java
+++ northwindj/northwindj/src/main/java/com/tao/northwindj/web/controllers/MasterController.java
@@ -1,14 +1,18 @@
 package com.tao.northwindj.web.controllers;
 
+import java.util.Vector;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.tao.northwindj.domains.Result;
 import com.tao.northwindj.domains.SimpleMasterObject;
+import com.tao.northwindj.domains.master.City;
 import com.tao.northwindj.domains.master.Country;
 import com.tao.northwindj.repositories.imp.MasterRepository;
 
@@ -25,6 +29,18 @@ public class MasterController {
 	{
 		Result<SimpleMasterObject> result = mRepository.findAll(Country.class);
 		return result.getFullResult();
+	}
+	@RequestMapping(value="/city/all",method=RequestMethod.GET)
+	@Transactional
+	public @ResponseBody Result<SimpleMasterObject> getAllCity(@RequestParam Long id)
+	{
+		Vector<String> fields = new Vector<String>();
+		fields.add("countryId");
+		Vector<Object> values = new Vector<Object>();
+		values.add(id);
+		Result<SimpleMasterObject> result = mRepository.findAll(City.class, fields, values);
+		return result.getFullResult();
+		
 	}
 
 }
