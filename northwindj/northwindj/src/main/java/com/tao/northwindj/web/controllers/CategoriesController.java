3293bb82048300373a81a12d956f1ab94d81fed6
anawatj <ajarusiripot@gmail.com>
2015-11-19 13:22:52 +0700

    CategoriesController.java

---
+++ northwindj/northwindj/src/main/java/com/tao/northwindj/web/controllers/CategoriesController.java
@@ -0,0 +1,59 @@
+package com.tao.northwindj.web.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.tao.northwindj.domains.Result;
+import com.tao.northwindj.domains.categories.Categories;
+import com.tao.northwindj.repositories.ICategoriesRepository;
+
+@Controller
+@RequestMapping(value="/categories")
+public class CategoriesController {
+	
+	@Autowired
+	private ICategoriesRepository cRepository;
+	
+	@RequestMapping(value="/all",method=RequestMethod.GET)
+	@Transactional
+	public @ResponseBody Result<Categories> getAll()
+	{
+		Result<Categories> result = cRepository.findAll();
+		return result.getFullResult();
+	}
+	@RequestMapping(value="/single",method=RequestMethod.GET)
+	@Transactional
+	public @ResponseBody Categories getById(@RequestParam Long id)
+	{
+		Categories result = cRepository.findById(id);
+		if(result ==null)
+		{
+			return new Categories();
+		}else
+		{
+			return result;
+		}
+		
+	}
+	@RequestMapping(value="/save",method=RequestMethod.POST)
+	@Transactional
+	public @ResponseBody Categories save(@RequestBody Categories entity)
+	{
+		Categories result = cRepository.save(entity);
+		return result;
+	}
+	@RequestMapping(value="/delete",method=RequestMethod.POST)
+	@Transactional
+	public @ResponseBody String delete(@RequestBody Categories entity)
+	{
+		cRepository.remove(entity.getId());
+		return "Success";
+		
+	}
+}
